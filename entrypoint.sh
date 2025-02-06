dbe554c50d620933ae07f0158ff2eaef86d57acd
thanhngan.tran <thanhngan.tran@maibornwolff.de>
2025-02-06 18:43:10 +0100

    feat: added output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,6 @@
 #! /bin/bash
 ARGS=("$@")
 
-cp $1 .
-
-ls -a
 python /mw-converter-trivy/convert-mw-policy-to-trivy.py ${ARGS[*]}
 
-#trivy=$(cat $2)
-#echo "trivy-yaml=$trivy" >> $GITHUB_OUTPUT
+echo "trivy-yaml=$2" >> $GITHUB_OUTPUT
