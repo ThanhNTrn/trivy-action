245b0f46144df5075e13485a3829a87db49e1c3d
thanhngan.tran <thanhngan.tran@maibornwolff.de>
2025-02-07 10:26:59 +0100

    feat: clean up removed snd parameter

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 ARGS=("$@")
 
-python /mw-converter-trivy/convert-mw-policy-to-trivy.py ${ARGS[*]}
+python /mw-converter-trivy/convert-mw-policy-to-trivy.py "${ARGS[*]}"
 
 OUTPUT_FILE="./trivy.yaml"
-echo "trivy-yaml=$OUTPUT_FILE" >> $GITHUB_OUTPUT
+echo "trivy-yaml=$OUTPUT_FILE" >> "$GITHUB_OUTPUT"
